761cd281e8be3f63fcd76d65de18109d06e56fd1
moshinzamani <mohsenzamanist@gmail.com>
2023-11-14 01:06:39 +0000

    Pesrsonal first commit

--- src/components/Personal.tsx
+++ src/components/Personal.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { AiOutlineCalendar } from "react-icons/ai";
+import mo from "../assets/Mo.jpg";
+import "../style.css";
 
 const Personal: React.FC = () => {
-  return <div>Personal</div>;
+  return (
+    <div className="personal_main">
+      <div className="personal_sidebar">
+        <div className="personal_sidebar_top">
+          <img className="personal_image" src={mo} alt="Profile Image" />
+        </div>
+        <div className="personal_sidebar_bottom">
+          <h3 className="today_task">
+            <AiOutlineCalendar />
+            Today tasks
+          </h3>
+          <h3 className="completed_task">Completed tasks</h3>
+        </div>
+      </div>
+      <div className="personal_main_content"></div>
+    </div>
+  );
 };
 
 export default Personal;
